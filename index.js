ffc7cd9eebf8874aa8f32a72ccf27075a6ca4b62
Franjo <72498227+Quelorel@users.noreply.github.com>
2023-02-08 15:08:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ headerToggle.addEventListener("click", toggleMobileMenu);
 
 const pricingList = document.querySelectorAll(".pricing-list");
 const pricingListHeightArray = [];
+const pricingTitle = document.querySelectorAll(".pricing-title");
+const pricingTitleHeightArray = [];
 
 function mediaQueryFunction() {
   if (mediaQuery.matches) {
@@ -24,17 +26,29 @@ function mediaQueryFunction() {
       navItem[i].addEventListener("click", toggleMobileMenu);
     }
   } else {
-    for (let i = 0; i < pricingList.length; i++) {
-      pricingListHeightArray.push(pricingList[i].offsetHeight);
-      console.log(pricingList[i].offsetHeight);
-    }
-    heighestElement = Math.max(...pricingListHeightArray);
-    pricingList.forEach((element) => {
-      element.style.height = heighestElement;
-      console.log(element.offsetHeight);
-    });
   }
+  for (let i = 0; i < pricingList.length; i++) {
+    pricingListHeightArray.push(pricingList[i].offsetHeight);
+  }
+  heighestElement = Math.max(...pricingListHeightArray);
+  pricingList.forEach((element) => {
+    element.style.height = heighestElement;
+  });
+
+  for (let i = 0; i < pricingTitle.length; i++) {
+    pricingTitleHeightArray.push(pricingTitle[i].offsetHeight);
+  }
+  heighestTitle = Math.max(...pricingTitleHeightArray);
+  pricingTitle.forEach((element) => {
+    element.style.height = heighestTitle;
+  });
 }
+
+const heightAdjust = function () {
+  mediaQueryFunction();
+};
+
+addEventListener("resize", heightAdjust);
 
 const mediaQuery = window.matchMedia("(max-width: 1100px)");
 mediaQueryFunction(mediaQuery); // Call listener function at run time
